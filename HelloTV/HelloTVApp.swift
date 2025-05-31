957c6faebdb3c6eec8762b40c8cde29f9efdaf29
Mark Whitaker <mark@bitrot.net>
2025-05-31 10:02:03 +0100

    Initial Commit

---
+++ HelloTV/HelloTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTVApp.swift
+//  HelloTV
+//
+//  Created by Mark Whitaker on 31/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
